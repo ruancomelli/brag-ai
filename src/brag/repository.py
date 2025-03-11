82edac134358d63529d477f3ba6a227233d36aad
Ruan Comelli <ruancomelli@gmail.com>
2025-03-11 16:16:13 -0300

    refactor: replace `typer` with `cryclopts`

--- src/brag/repository.py
+++ src/brag/repository.py
@@ -1,8 +1,13 @@
 """Defines a data structure for representing a repository."""
 
-from typing import Self
+from typing import Annotated, Self
 
-from pydantic import BaseModel
+from pydantic import BaseModel, StringConstraints
+
+type RepoFullName = Annotated[
+    str,
+    StringConstraints(pattern=r"^[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+$"),
+]
 
 
 class RepoReference(BaseModel):
@@ -22,17 +27,14 @@ class RepoReference(BaseModel):
         return f"{self.owner}/{self.name}"
 
     @classmethod
-    def from_repo_full_name(cls, repo_full_name: str) -> Self:
-        """Create a RepoReference object from a full repository name string.
+    def from_repo_full_name(cls, repo_full_name: RepoFullName) -> Self:
+        """Create a RepoReference object from a RepoFullName object.
 
         Args:
-            repo_full_name: The full name of the repository in the format 'owner/name'.
+            repo_full_name: A RepoFullName object.
 
         Returns:
             A RepoReference object.
-
-        Raises:
-            ValueError: If the repository full name is not in the correct format.
         """
         owner, name = repo_full_name.split("/")
         return cls(owner=owner, name=name)
